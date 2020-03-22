4257ee2ced5e8aebcdc02c83ca91076ab0464298
roverv <robvervoort@gmail.com>
2020-03-22 20:14:19 +0100

    Tab and row action buttons restyling (icons removed)

--- app/src/components/TableData.vue
+++ app/src/components/TableData.vue
@@ -81,38 +81,18 @@
             </div>
 
             <a class="rows-action" href="" v-if="selected_rows.length == 1">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 mr-2 fill-current text-gray-600">
-                <path class="text-gray-600"
-                      d="M4 14a1 1 0 0 1 .3-.7l11-11a1 1 0 0 1 1.4 0l3 3a1 1 0 0 1 0 1.4l-11 11a1 1 0 0 1-.7.3H5a1 1 0 0 1-1-1v-3z" />
-                <rect width="20" height="2" x="2" y="20" class="text-gray-500" rx="1" />
-              </svg>
               <span>Edit</span>
             </a>
 
             <a class="rows-action" href="">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 mr-2 fill-current text-gray-600">
-                <rect width="14" height="14" x="3" y="3" class="text-gray-500" rx="2" />
-                <rect width="14" height="14" x="7" y="7" class="text-gray-600" rx="2" />
-              </svg>
               <span>Duplicate</span>
             </a>
 
             <a class="rows-action" @click="deleteRows()">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 mr-2 fill-current text-gray-600">
-                <path class="text-gray-600"
-                      d="M5 5h14l-.89 15.12a2 2 0 0 1-2 1.88H7.9a2 2 0 0 1-2-1.88L5 5zm5 5a1 1 0 0 0-1 1v6a1 1 0 0 0 2 0v-6a1 1 0 0 0-1-1zm4 0a1 1 0 0 0-1 1v6a1 1 0 0 0 2 0v-6a1 1 0 0 0-1-1z" />
-                <path class="text-gray-500"
-                      d="M8.59 4l1.7-1.7A1 1 0 0 1 11 2h2a1 1 0 0 1 .7.3L15.42 4H19a1 1 0 0 1 0 2H5a1 1 0 1 1 0-2h3.59z" />
-              </svg>
               <span>Delete</span>
             </a>
 
             <a class="rows-action" href="">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 mr-2 fill-current text-gray-600">
-                <path class="text-gray-600"
-                      d="M6 2h6v6c0 1.1.9 2 2 2h6v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm2 11a1 1 0 0 0 0 2h8a1 1 0 0 0 0-2H8zm0 4a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2H8z" />
-                <polygon class="text-gray-500" points="14 2 20 8 14 8" />
-              </svg>
               <span>Export</span>
             </a>
 
@@ -495,11 +475,13 @@
   }
 
   .rows-action {
-    @apply mx-2 inline-flex rounded-lg py-1 px-2 items-center;
+    @apply  inline-flex px-6 leading-tight items-center text-light-300 border-r border-light-100;
   }
-
+  .rows-action:last-child {
+    @apply border-0;
+  }
   .rows-action:hover {
-    @apply bg-light-100;
+    @apply underline;
   }
 
 </style>
--- app/src/components/TableNav.vue
+++ app/src/components/TableNav.vue
@@ -3,37 +3,17 @@
     <h2 class=" text-xl">{{ tableid }}</h2>
     <div>
       <router-link :to="{ name: 'table', params: { tableid: tableid } }" class="subnav-item"  :class="{ 'active' : ($route.name == 'table') }">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-4 fill-current mr-2 text-gray-400">
-          <rect width="20" height="12" x="2" y="10" class="primary" rx="2"></rect>
-          <path class="text-gray-400"
-                d="M20 8H4c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2zm-2-4H6c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2z"></path>
-        </svg>
         Data
       </router-link>
 
       <router-link :to="{ name: 'structure', params: { tableid: tableid } }" class="subnav-item" :class="{ 'active' : ($route.name == 'structure') }">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-4 fill-current mr-2 text-gray-400">
-          <path class="primary"
-                d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v10h2a2 2 0 0 1 2 2c0 1.1.9 2 2 2h2a2 2 0 0 0 2-2c0-1.1.9-2 2-2h2V5H5z"></path>
-          <path class="text-gray-600"
-                d="M8 7h8a1 1 0 0 1 0 2H8a1 1 0 1 1 0-2zm0 4h8a1 1 0 0 1 0 2H8a1 1 0 0 1 0-2z"></path>
-        </svg>
         Structure
       </router-link>
       <a class="subnav-item">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-4 fill-current mr-2 text-gray-400">
-          <polygon class="primary" points="3 6 12 2 21 6 21 18 12 22 3 18"></polygon>
-          <path class="text-gray-600" d="M21 6v12l-9 4V10l9-4zM9 8.67v3l-3-1.34v-3l9-4 3 1.34-9 4z"></path>
-        </svg>
         Indexes
       </a>
 
       <a class="subnav-item">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-4 fill-current mr-2 text-gray-400">
-          <circle cx="12" cy="12" r="10" class="primary"></circle>
-          <path class="text-gray-600"
-                d="M13 11h4a1 1 0 0 1 0 2h-4v4a1 1 0 0 1-2 0v-4H7a1 1 0 0 1 0-2h4V7a1 1 0 0 1 2 0v4z"></path>
-        </svg>
         Add row
       </a>
     </div>
@@ -71,13 +51,16 @@
   }
 
   .subnav-item {
-    @apply mx-2 inline-flex rounded-lg py-1 px-2 items-center;
+    @apply  inline-flex px-6 leading-tight items-center text-light-300 border-r border-light-100;
+  }
+  .subnav-item:last-child {
+    @apply border-0;
   }
   .subnav-item:hover {
-    @apply bg-light-100;
+    @apply underline;
   }
   .subnav-item.active {
-    @apply px-3 border-2 border-light-200 bg-light-100;
+    @apply text-highlight-700;
   }
 
 </style>
