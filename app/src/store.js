6829d3a4d3179d7ed1f3a16b8e92d2759d95fbeb
roverv <robvervoort@gmail.com>
2020-04-02 21:08:35 +0200

    Vuex-persistedstate store plugin for saving store data to session storage

--- app/src/store.js
+++ app/src/store.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+
 import DatabasesStore from './store/databases';
 import TablesStore from './store/tables';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
+  plugins: [createPersistedState({ storage: window.sessionStorage })],
 
   modules: {
     databases: DatabasesStore,
