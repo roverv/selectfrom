9cff32f101af55d87711c1585edd0dd6314fc935
roverv <robvervoort@gmail.com>
2020-04-06 19:10:36 +0200

    Query: removecomments from sql function, split sql text into queries function

--- api/query.php
+++ api/query.php
@@ -2,8 +2,16 @@
 
     require_once '_header.php';
 
-        $query = $_POST['query'];
-//    $query = "SELECT * FROM user JOIN organisation ON user.organisation_id = organisation.id LIMIT 5;";
+
+    $query = $_POST['query'];
+    //        $query = "SELECT * FROM user JOIN organisation ON user.organisation_id = organisation.id LIMIT 5;";
+    //        $query = "DELETE FROM country;
+    //INSERT INTO country(code, name, name_nl) VALUES ('NL', 'netherlands', 'Nederland');
+    //INSERT INTO country(code, name, name_nl) VALUES ('DE', 'Germany', 'Duitsland');";
+
+    $query   = removeComments($query);
+    $queries = split_sql($query);
+
 
     try {
         $q = $pdo->query($query);
@@ -23,7 +31,7 @@
       'result'        => 'success',
       'affected_rows' => $q->rowCount(),
       'rows'          => $q->fetchAll(PDO::FETCH_NUM),
-      'columns_meta'   => $columns_meta,
+      'columns_meta'  => $columns_meta,
     ]);
     exit;
 
@@ -42,27 +50,50 @@
 
     exit;
 
+    /**
+     * Taken from: https://stackoverflow.com/questions/4747808/split-mysql-queries-in-array-each-queries-separated-by
+     *
+     * @param $sql_text
+     * @return array|mixed
+     */
+    function split_sql($sql_text)
+    {
+        // Return array of ; terminated SQL statements in $sql_text.
+        $re_split_sql = '%(?#!php/x re_split_sql Rev:20170816_0600)
+        # Match an SQL record ending with ";"
+        \s*                                     # Discard leading whitespace.
+        (                                       # $1: Trimmed non-empty SQL record.
+          (?:                                   # Group for content alternatives.
+            \'[^\'\\\\]*(?:\\\\.[^\'\\\\]*)*\'  # Either a single quoted string,
+          | "[^"\\\\]*(?:\\\\.[^"\\\\]*)*"      # or a double quoted string,
+          | /\*[^*]*\*+(?:[^*/][^*]*\*+)*/      # or a multi-line comment,
+          | \#.*                                # or a # single line comment,
+          | --.*                                # or a -- single line comment,
+          | [^"\';#]                            # or one non-["\';#-]
+          )+                                    # One or more content alternatives
+          (?:;|$)                               # Record end is a ; or string end.
+        )                                       # End $1: Trimmed SQL record.
+        %x';  // End $re_split_sql.
+        if (preg_match_all($re_split_sql, $sql_text, $matches)) {
+            return $matches[1];
+        }
 
-    $result      = @($unbuffered ? mysql_unbuffered_query($query, $this->_link) : mysql_query($query,
-      $this->_link)); // @ - mute mysql.trace_mode
-    $this->error = "";
-    if (!$result) {
-        $this->errno = mysql_errno($this->_link);
-        $this->error = mysql_error($this->_link);
-
-        return false;
+        return array();
     }
-    if ($result === true) {
-        $this->affected_rows = mysql_affected_rows($this->_link);
-        $this->info          = mysql_info($this->_link);
 
-        return true;
+    /**
+     * Taken from: https://stackoverflow.com/questions/9690448/regular-expression-to-remove-comments-from-sql-statement
+     *
+     * @param $sql_text
+     * @return string
+     */
+    function removeComments($sql_text)
+    {
+        $regex = '/["\'`][^"\'`]*(?!\\\\)["\'`](*SKIP)(*F)|(?m-s:\s*(?:\-{2}|\#)[^\n]*$)|(?:\/\*.*?\*\/(?(?=(?m-s:[\t ]+$))[\t ]+))/s';
+        // alternative, not using PCRE:
+        // $RXSQLComments = '@(--[^\r\n]*)|(\#[^\r\n]*)|(/\*[\w\W]*?(?=\*/)\*/)@ms';
+        $result = preg_replace($regex, '', $sql_text);
+
+        return $result;
     }
-
-
-    //    $result = $connection->store_result();
-    var_dump($rows);
-
-    echo json_encode($rows);
-    exit;
 
