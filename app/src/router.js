a795fac7163bffd2beccddce2467a08984296ff5
roverv <robvervoort@gmail.com>
2020-04-13 20:20:08 +0200

    Removed unused and test files

--- app/src/router.js
+++ app/src/router.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/test',
-      name: 'test',
-      component: () => import(/* webpackChunkName: "about" */ './views/Test.vue')
-    },
     {
       path: '/server',
       alias: '/', //todo: change later to login screen
@@ -18,7 +12,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Server.vue')
+      component: () => import(/* webpackChunkName: "server" */ './views/Server.vue')
     },
     {
       path: '/database/:database',
@@ -26,43 +20,43 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Database.vue'),
+      component: () => import(/* webpackChunkName: "database" */ './views/Database.vue'),
       props: true,
     },
     {
       path: '/table/:tableid',
       name: 'table',
-      component: () => import(/* webpackChunkName: "about" */ './views/TableData.vue'),
+      component: () => import(/* webpackChunkName: "tabledata" */ './views/TableData.vue'),
       props: true,
     },
     {
       path: '/table/:tableid/:column',
       name: 'tablewithcolumn',
-      component: () => import(/* webpackChunkName: "about" */ './views/TableData.vue'),
+      component: () => import(/* webpackChunkName: "tabledata" */ './views/TableData.vue'),
       props: true,
     },
     {
       path: '/table/:tableid/:column/:value',
       name: 'tablewithcolumnvalue',
-      component: () => import(/* webpackChunkName: "about" */ './views/TableData.vue'),
+      component: () => import(/* webpackChunkName: "tabledata" */ './views/TableData.vue'),
       props: true,
     },
     {
       path: '/structure/:tableid',
       name: 'structure',
-      component: () => import(/* webpackChunkName: "about" */ './views/TableStructure.vue'),
+      component: () => import(/* webpackChunkName: "tablestructure" */ './views/TableStructure.vue'),
       props: true,
     },
     {
       path: '/editrow/:tableid/:column/:rowid',
       name: 'editrow',
-      component: () => import(/* webpackChunkName: "about" */ './views/EditRow.vue'),
+      component: () => import(/* webpackChunkName: "editrow" */ './views/EditRow.vue'),
       props: true,
     },
     {
       path: '/query',
       name: 'query',
-      component: () => import(/* webpackChunkName: "about" */ './views/Query.vue'),
+      component: () => import(/* webpackChunkName: "query" */ './views/Query.vue'),
       props: true,
     },
   ]
--- app/src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an 22 thsi page</h1>
-  </div>
-</template>
