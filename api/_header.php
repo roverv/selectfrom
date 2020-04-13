e7a4d89bc2e4eee8b45b40ee09b7ee84be3bc160
roverv <robvervoort@gmail.com>
2020-04-13 16:23:40 +0200

    Escape mysql identifier function

--- api/_header.php
+++ api/_header.php
@@ -6,21 +6,37 @@
     header('Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 
-    $host = 'localhost';
-    $db   = $_GET['db'] ?? '';
-    $user = 'root';
-    $pass = '';
+    $host    = 'localhost';
+    $db      = $_GET['db'] ?? '';
+    $user    = 'root';
+    $pass    = '';
     $charset = 'utf8mb4';
 
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $dsn     = "mysql:host=$host;dbname=$db;charset=$charset";
     $options = [
       PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::ATTR_EMULATE_PREPARES   => false, // this must be true if we want to run running multiple queries from one sql text
+      PDO::ATTR_EMULATE_PREPARES   => true,
+      // this must be true if we want to run running multiple queries from one sql text
     ];
     try {
         $pdo = new PDO($dsn, $user, $pass, $options);
     } catch (\PDOException $e) {
         throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+
+
+    /**
+     * Escape query identifiers like tablename, table column or database name
+     * use as: UPDATE user SET escapeWithBacktick('name') = :name
+     * See: https://phpdelusions.net/pdo/sql_injection_example and https://phpdelusions.net/pdo_examples/insert_helper
+     * NOTE: MYSQL ONLY!
+     *
+     * @param $name
+     * @return string
+     */
+    function escape_mysql_identifier($field)
+    {
+        return "`" . str_replace("`", "``", $field) . "`";
     }
 
