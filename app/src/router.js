7426948b8e9c94abb83c99c09ec09794a6f9dc0d
Koen Dielis <koen@geenensd.com>
2020-05-08 10:43:41 +0200

    Middleware: redirect to page if active database is not set

--- app/src/router.js
+++ app/src/router.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Store from './store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/server',
@@ -28,49 +29,88 @@ export default new Router({
       name: 'table',
       component: () => import(/* webpackChunkName: "tabledata" */ './views/TableData.vue'),
       props: true,
+      meta: {
+        requiresActiveDatabase: true
+      }
     },
     {
       path: '/table/:tableid/:column',
       name: 'tablewithcolumn',
       component: () => import(/* webpackChunkName: "tabledata" */ './views/TableData.vue'),
       props: true,
+      meta: {
+        requiresActiveDatabase: true
+      }
     },
     {
       path: '/table/:tableid/:column/:value',
       name: 'tablewithcolumnvalue',
       component: () => import(/* webpackChunkName: "tabledata" */ './views/TableData.vue'),
       props: true,
+      meta: {
+        requiresActiveDatabase: true
+      }
     },
     {
       path: '/structure/:tableid',
       name: 'structure',
       component: () => import(/* webpackChunkName: "tablestructure" */ './views/TableStructure.vue'),
       props: true,
+      meta: {
+        requiresActiveDatabase: true
+      }
     },
     {
       path: '/editrow/:tableid/:column/:rowid',
       name: 'editrow',
       component: () => import(/* webpackChunkName: "editrow" */ './views/EditRow.vue'),
       props: true,
+      meta: {
+        requiresActiveDatabase: true
+      }
     },
     {
       path: '/addrow/:tableid',
       name: 'addrow',
       component: () => import(/* webpackChunkName: "editrow" */ './views/EditRow.vue'),
       props: true,
+      meta: {
+        requiresActiveDatabase: true
+      }
     },
     {
       path: '/query',
       name: 'query',
       component: () => import(/* webpackChunkName: "query" */ './views/Query.vue'),
       props: true,
+      meta: {
+        requiresActiveDatabase: true
+      }
     },
     {
       path: '/query/history/:historyindex',
       name: 'queryhistory',
       component: () => import(/* webpackChunkName: "query" */ './views/Query.vue'),
       props: true,
+      meta: {
+        requiresActiveDatabase: true
+      }
     },
   ]
 });
 
+router.beforeEach((to, from, next) => {
+  // if this route requires active database
+  if (to.matched.some(record => record.meta.requiresActiveDatabase)) {
+    // redirect to database list if active database is not set
+    if (typeof Store.state.activeDatabase === 'undefined' || Store.state.activeDatabase == '') {
+      next({name: 'server'});
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+export default router;
