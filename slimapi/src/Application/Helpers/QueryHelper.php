5428200673efd3254ac40dcd55e2ccf6983af0ef
roverv <robvervoort@gmail.com>
2021-02-08 21:08:13 +0100

    Splitting query string completely rewritten

--- slimapi/src/Application/Helpers/QueryHelper.php
+++ slimapi/src/Application/Helpers/QueryHelper.php
@@ -16,115 +16,151 @@ class QueryHelper
      * @param $name
      * @return string
      */
-    public static function escapeMysqlId($field)
-    {
+    public static function escapeMysqlId($field) {
         return "`".str_replace("`", "``", $field)."`";
     }
 
     /**
-     * Taken from: https://stackoverflow.com/questions/147821/loading-sql-files-from-within-php
-     * Note: expects trim() to have already been run on $sql.
+     * Split SQL string
+     * see: https://stackoverflow.com/a/15354406/1856264
      *
-     * @param $sql
-     * @param $delimiter
-     * @return array
+     * @param  string path to sql file
      */
-    public static function splitSql($sql, $delimiter = ";") {
-        // Split up our string into "possible" SQL statements.
-        $tokens = explode($delimiter, $sql);
+    public static function splitSql($sql_string) {
+        // see: https://stackoverflow.com/questions/1462720/iterate-over-each-line-in-a-string-in-php
+        // write to temporary file for fast and easy access, also no need for splitting on linebreaks with regex
+        $file = fopen("php://temp", 'r+');
+        fputs($file, $sql_string);
+        rewind($file);
 
-        // try to save mem.
-        $sql    = "";
-        $output = array();
+        $delimiter             = ';';
+        $is_first_row          = true;
+        $is_multi_line_comment = false;
+        $sql                   = '';
 
-        // we don't actually care about the matches preg gives us.
-        $matches = array();
+        $queries = [];
 
-        // this is faster than calling count($oktens) every time thru the loop.
-        $token_count = count($tokens);
-        for ($i = 0; $i < $token_count; $i++) {
-            // Don't wanna add an empty string as the last thing in the array.
-            if (($i != ($token_count - 1)) || (strlen($tokens[$i]) > 0)) {
-                // This is the total number of single quotes in the token.
-                $total_quotes = preg_match_all("/'/", $tokens[$i], $matches);
-                // Counts single quotes that are preceded by an odd number of backslashes,
-                // which means they're escaped quotes.
-                $escaped_quotes = preg_match_all("/(?<!\\\\)(\\\\\\\\)*\\\\'/", $tokens[$i], $matches);
+        while (!feof($file)) {
+            $row = fgets($file);
 
-                $unescaped_quotes = $total_quotes - $escaped_quotes;
-
-                // If the number of unescaped quotes is even, then the delimiter did NOT occur inside a string literal.
-                if (($unescaped_quotes % 2) == 0) {
-                    // It's a complete sql statement.
-                    $output[] = $tokens[$i];
-                    // save memory.
-                    $tokens[$i] = "";
-                } else {
-                    // incomplete sql statement. keep adding tokens until we have a complete one.
-                    // $temp will hold what we have so far.
-                    $temp = $tokens[$i].$delimiter;
-                    // save memory..
-                    $tokens[$i] = "";
-
-                    // Do we have a complete statement yet?
-                    $complete_stmt = false;
-
-                    for ($j = $i + 1; (!$complete_stmt && ($j < $token_count)); $j++) {
-                        // This is the total number of single quotes in the token.
-                        $total_quotes = preg_match_all("/'/", $tokens[$j], $matches);
-                        // Counts single quotes that are preceded by an odd number of backslashes,
-                        // which means they're escaped quotes.
-                        $escaped_quotes = preg_match_all("/(?<!\\\\)(\\\\\\\\)*\\\\'/", $tokens[$j], $matches);
-
-                        $unescaped_quotes = $total_quotes - $escaped_quotes;
-
-                        if (($unescaped_quotes % 2) == 1) {
-                            // odd number of unescaped quotes. In combination with the previous incomplete
-                            // statement(s), we now have a complete statement. (2 odds always make an even)
-                            $output[] = $temp.$tokens[$j];
-
-                            // save memory.
-                            $tokens[$j] = "";
-                            $temp       = "";
-
-                            // exit the loop.
-                            $complete_stmt = true;
-                            // make sure the outer loop continues at the right point.
-                            $i = $j;
-                        } else {
-                            // even number of unescaped quotes. We still don't have a complete statement.
-                            // (1 odd and 1 even always make an odd)
-                            $temp .= $tokens[$j].$delimiter;
-                            // save memory.
-                            $tokens[$j] = "";
-                        }
-                    } // for..
-                } // else
+            // remove BOM for utf-8 encoded file
+            if ($is_first_row) {
+                $row          = preg_replace('/^\x{EF}\x{BB}\x{BF}/', '', $row);
+                $is_first_row = false;
             }
+
+            // 1. ignore empty string and comment row
+            if (trim($row) == '' || preg_match('/^\s*(#|--\s)/sUi', $row)) {
+                continue;
+            }
+
+            // 2. clear comments
+            $row = trim(self::removeComments($row, $is_multi_line_comment));
+
+            // 3. parse delimiter row
+            if (preg_match('/^DELIMITER\s+[^ ]+/sUi', $row)) {
+                $delimiter = preg_replace('/^DELIMITER\s+([^ ]+)$/sUi', '$1', $row);
+                continue;
+            }
+
+            // 4. separate sql queries by delimiter
+            $offset = 0;
+            while (strpos($row, $delimiter, $offset) !== false) {
+                $delimiter_offset = strpos($row, $delimiter, $offset);
+                if (self::isQuoted($delimiter_offset, $row)) {
+                    $offset = $delimiter_offset + strlen($delimiter);
+                } else {
+                    $sql       = trim($sql.' '.trim(substr($row, 0, $delimiter_offset)));
+                    $queries[] = $sql;
+
+                    $row    = substr($row, $delimiter_offset + strlen($delimiter));
+                    $offset = 0;
+                    $sql    = '';
+                }
+            }
+            $sql = trim($sql.' '.$row);
+        }
+        if (strlen($sql) > 0) {
+            $queries[] = $row;
         }
 
-        return $output;
+        fclose($file);
+
+        return $queries;
     }
 
     /**
-     * Taken from: https://stackoverflow.com/questions/9690448/regular-expression-to-remove-comments-from-sql-statement
+     * Remove comments from sql
      *
-     * @param $sql_text
+     * @param  string sql
+     * @param  boolean is multicomment line
      * @return string
      */
-    public static function removeComments($sql_text) {
-        $re = '/["\'`][^"\'`]*(?!\\\\)["\'`](*SKIP)(*F)       # Make sure we\'re not matching inside of quotes, double quotes or backticks
-|(?m-s:\s*(?:\-{2}|\#)[^\n]*$) # Single line comment
-|(?:
-  # \/\*.*?\*\/                  # Multi-line comment
-  (?(?=(?m-s:[\t ]+$))         # Get trailing whitespace if any exists and only if it\'s the rest of the line
-    [\t ]+
-  )
-)/xs';
+    private static function removeComments($sql, &$is_multi_line_comment) {
+        if ($is_multi_line_comment) {
+            if (preg_match('#\*/#sUi', $sql)) {
+                $sql                   = preg_replace('#^.*\*/\s*#sUi', '', $sql);
+                $is_multi_line_comment = false;
+            } else {
+                $sql = '';
+            }
+            if (trim($sql) == '') {
+                return $sql;
+            }
+        }
 
-        $result = preg_replace($re, '', $sql_text);
+        $offset = 0;
+        while (preg_match('{--\s|#|/\*[^!]}sUi', $sql, $matched, PREG_OFFSET_CAPTURE, $offset)) {
+            list($comment, $found_on) = $matched[0];
+            if (self::isQuoted($found_on, $sql)) {
+                $offset = $found_on + strlen($comment);
+            } else {
+                if (substr($comment, 0, 2) == '/*') {
+                    $closed_on = strpos($sql, '*/', $found_on);
+                    if ($closed_on !== false) {
+                        $sql = substr($sql, 0, $found_on).substr($sql, $closed_on + 2);
+                    } else {
+                        $sql                   = substr($sql, 0, $found_on);
+                        $is_multi_line_comment = true;
+                    }
+                } else {
+                    $sql = substr($sql, 0, $found_on);
+                    break;
+                }
+            }
+        }
 
-        return $result;
+        return $sql;
+    }
+
+    /**
+     * Check if "offset" position is quoted
+     *
+     * @param  int  $offset
+     * @param  string  $text
+     * @return boolean
+     */
+    private static function isQuoted($offset, $text) {
+        if ($offset > strlen($text)) {
+            $offset = strlen($text);
+        }
+
+        // the text up until the delimiter
+        $sub_text    = substr($text, 0, $offset);
+        $quote_count = substr_count($sub_text, "'");
+        if ($quote_count === 0) {
+            return false;
+        }
+
+        $escaped_quote_count = substr_count($sub_text, "\'");
+        // subtract escaped quotes as these do not count
+        $quote_count -= $escaped_quote_count;
+        // if the number of quotes is uneven, it means the delimiter is quoted, eg "INSERT INTO `test` VALUES('test case; abcd');"
+        if ($quote_count % 2 === 1) {
+            return true;
+        }
+
+        return false;
     }
 
 
@@ -135,18 +171,15 @@ class QueryHelper
      * @param  string  $sql  SQL query statement (or an array with parts)
      * @return string
      */
-    public static function getQueryType($sql)
-    {
+    public static function getQueryType($sql) {
         if (is_array($sql)) {
             $sql = key($sql);
         }
 
         $matches = null;
-        if (!preg_match(
-          '/^\s*(SELECT|INSERT|REPLACE|UPDATE|DELETE|TRUNCATE|CALL|DO|HANDLER|LOAD\s+(?:DATA|XML)\s+INFILE|(?:ALTER|CREATE|DROP|RENAME)\s+(?:DATABASE|TABLE|VIEW|FUNCTION|PROCEDURE|TRIGGER|INDEX)|PREPARE|EXECUTE|DEALLOCATE\s+PREPARE|DESCRIBE|EXPLAIN|HELP|USE|LOCK\s+TABLES|UNLOCK\s+TABLES|SET|SHOW|START\s+TRANSACTION|BEGIN|COMMIT|ROLLBACK|SAVEPOINT|RELEASE SAVEPOINT|CACHE\s+INDEX|FLUSH|KILL|LOAD|RESET|PURGE\s+BINARY\s+LOGS|START\s+SLAVE|STOP\s+SLAVE)\b/si',
-          $sql,
-          $matches
-        )) {
+        if (!preg_match('/^\s*(SELECT|INSERT|REPLACE|UPDATE|DELETE|TRUNCATE|CALL|DO|HANDLER|LOAD\s+(?:DATA|XML)\s+INFILE|(?:ALTER|CREATE|DROP|RENAME)\s+(?:DATABASE|TABLE|VIEW|FUNCTION|PROCEDURE|TRIGGER|INDEX)|PREPARE|EXECUTE|DEALLOCATE\s+PREPARE|DESCRIBE|EXPLAIN|HELP|USE|LOCK\s+TABLES|UNLOCK\s+TABLES|SET|SHOW|START\s+TRANSACTION|BEGIN|COMMIT|ROLLBACK|SAVEPOINT|RELEASE SAVEPOINT|CACHE\s+INDEX|FLUSH|KILL|LOAD|RESET|PURGE\s+BINARY\s+LOGS|START\s+SLAVE|STOP\s+SLAVE)\b/si',
+                        $sql,
+                        $matches)) {
             return null;
         }
 
